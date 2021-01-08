977d6c9f33f1a13a4380b3e117f723785128a98e
SilviaEPopescu <s.emilia.popescu@gmail.com>
2021-01-08 11:32:40 +0000

    11:32

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let playerMove = "rock";
 let computerMove = "scissors";
+
 if (playerMove === "rock" && computerMove === "paper") {  
  console.log("Computer Wins");
 }
@@ -27,3 +28,35 @@ if (playerMove === "scissors" && computerMove === "rock") {
 if (playerMove === "scissors" && computerMove === "scissors") {  
     console.log("Draw!");
    }
+
+   //Task2
+
+function game(playerMove, computerMove) {
+    if (playerMove === "rock" && computerMove === "paper") {  
+        return -1;
+       }
+       if (playerMove === "rock" && computerMove === "rock") {  
+        return 0;
+          }
+       if (playerMove === "rock" && computerMove === "scissors") {  
+        return 1;
+          }
+       if (playerMove === "paper" && computerMove === "rock") {  
+        return 1;
+          }
+       if (playerMove === "paper" && computerMove === "paper") {  
+        return 0;
+          }
+       if (playerMove === "paper" && computerMove === "scissors") {  
+        return -1;
+          }
+       if (playerMove === "scissors" && computerMove === "paper") {  
+        return 1;
+          }
+       if (playerMove === "scissors" && computerMove === "rock") {  
+        return -1;
+          }
+       if (playerMove === "scissors" && computerMove === "scissors") {  
+        return 0;
+          }
+}
